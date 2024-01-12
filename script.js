53249d1727047d6836f7ce4136cb25cd6a71c9ff
Yanmiii <132321184+Yanmiii@users.noreply.github.com>
2024-01-12 16:06:10 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const palabrasHorizontales = ["palabra1", "palabra2", "palabra3", "palabra4", "palabra5"];
+const palabrasVerticales = ["palabra6", "palabra7", "palabra8", "palabra9", "palabra10"];
+
+// Puedes agregar más palabras o personalizar según tus necesidades
